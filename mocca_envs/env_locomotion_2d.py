7f2020c8f84b7d249186b37f6f3acd2d566faf7f
Jessica Ma <gejessicama@gmail.com>
2021-02-08 14:29:59 -0800

    Hopefully this should give it steps as targets

--- mocca_envs/env_locomotion_2d.py
+++ mocca_envs/env_locomotion_2d.py
@@ -29,7 +29,7 @@ class Walker2DCustomEnv(EnvBase):
 
         # goal is to walk as far forward as possible
         # +x axis is forward, set to some large value
-        self.walk_target = np.array([1000, 0, 0])
+        self.walk_target = np.array([2, 0, 0])
 
         # Observation space is just robot's state, see robot.calc_state()
         high = np.inf * np.ones(self.robot.observation_space.shape[0])
@@ -100,6 +100,9 @@ class Walker2DCustomEnv(EnvBase):
             self._handle_keyboard()
             self.camera.track(pos=self.robot.body_xyz)
 
+        # Update target to be another 2 steps futher (Jessica)
+        self.walk_target = self.robot.body_xyz + np.array([2, 0, 0])
+
         # step() should return observation, reward, done (boolean), and info (dict)
         # info can be anything, some people return individual reward components
         # like, {"progress": self.progress, "energy": self.energy_penalty, ...}
@@ -108,6 +111,7 @@ class Walker2DCustomEnv(EnvBase):
     def calc_potential(self):
 
         walk_target_delta = self.walk_target - self.robot.body_xyz
+        print("walk target delta" + str(walk_target_delta))
 
         self.distance_to_target = (
             walk_target_delta[0] ** 2 + walk_target_delta[1] ** 2
